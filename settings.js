b645968306adbdeadf2c9addba00629ee6b09eb0
bres.love <love@bres.love>
2018-01-14 22:56:29 -0600

    hi

---
+++ settings.js
@@ -0,0 +1,9 @@
+var settings = {};
+
+settings.ALLOW_LOGIN = true;
+settings.HTML_ONLY = true;
+settings.DOMAIN = 'localhost';
+
+settings.COUCHDB_PREFIX = 'freeternity_';
+
+module.exports = settings;
