4ecea10cead6ad5bb2fe32ff3ecb219079b7f909
bres.love <love@bres.love>
2018-02-13 08:51:28 -0600

    hi

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { get, post } = server.router;
 const { render, json, redirect, status } = server.reply;
 
 const nunjucks = require('nunjucks');
-var env = nunjucks.configure('views');
+var env = nunjucks.configure('views'); 
 
 var options = {
     public: 'static',
@@ -35,6 +35,8 @@ nano.db.get(settings.COUCHDB_PREFIX+'treeternities', function(err, hi) {
 });
 
 var treeternities = nano.use(settings.COUCHDB_PREFIX+'treeternities');
+
+if (settings.LOCAL) {
 treeternities.insert({ title: "hi", uplifting: "uplifting", how: "areyouhi" }, function(err, body, header) {
   if (err) {
     console.log('[.insert] ', err.message);
@@ -42,6 +44,7 @@ treeternities.insert({ title: "hi", uplifting: "uplifting", how: "areyouhi" }, f
       console.log('you have added to the treeternity.', body)
   }
 });
+}
 
 var treeternities_each = [];
 
@@ -75,15 +78,56 @@ if (options.session.activated)
     settings.HTML_ONLY = false;
 
 server(options,[
-  get('/index.html', ctx => nunjucks.render('index_home.hbs', Object.assign({}, ctx.locals, 
+  get('/index.html', ctx => nunjucks.render('home.hbs', Object.assign({}, ctx.locals, 
                                                                   { chai: "chai", 
                                                                     settings: settings,
                                                                     options: options,
                                                                     treeternities: treeternities_each
                                                                   }))),
+
+    
+  get('/left-sidebar.html', ctx => nunjucks.render('left-sidebar.hbs', Object.assign({}, ctx.locals, 
+                                                                  { chai: "chai", 
+                                                                    settings: settings,
+                                                                    options: options,
+                                                                    treeternities: treeternities_each
+                                                                  }))),
+  get('/right-sidebar.html', ctx => nunjucks.render('right-sidebar.hbs', Object.assign({}, ctx.locals, 
+                                                                  { chai: "chai", 
+                                                                    settings: settings,
+                                                                    options: options,
+                                                                    treeternities: treeternities_each
+                                                                  }))),
+    
+  get('/no-sidebar.html', ctx => nunjucks.render('no-sidebar.hbs', Object.assign({}, ctx.locals, 
+                                                                  { chai: "chai", 
+                                                                    settings: settings,
+                                                                    options: options,
+                                                                    treeternities: treeternities_each
+                                                                  }))),  
+  get('/contact.html', ctx => nunjucks.render('contact.hbs', Object.assign({}, ctx.locals, 
+                                                                  { chai: "chai", 
+                                                                    settings: settings,
+                                                                    options: options,
+                                                                    treeternities: treeternities_each
+                                                                  }))),
+    /* email katherine at 
+    walter passmore, get contact info from katherine to walter.  
+    lots of organizations in texas and elsewhere 
+    alliance for community trees.  arbor day foundation, dana karcher
+    putting all orgs on a Map
+    california relief maps.
+    tree folks
+    when we plant where we plant is localized
+    who are the partners
+    davids birthday 
+    
+    
+    
+    small non-profit organizations */
   get('/', ctx => {
     return redirect('/index.html');
-  }),
+  }), 
   get('/activated.html', ctx => {
     options.session.activated = true;
     return redirect('/index.html?activated');
--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ settings.HTML_ONLY = true;
 settings.DOMAIN = 'localhost';
 
 settings.COUCHDB_PREFIX = 'treeternity_';
-
+settings.LOCAL = true;
 settings.EMAIL = 'volunteers@treeternity.com'
 
 module.exports = settings;
